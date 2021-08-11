376c74db5414846395bbaafbe5edcf5e87bb9f5f
Eric Balawejder <eric.balawejder@protonmail.com>
2021-08-11 12:33:35 -0400

    Initial content from start.spring.io and database files

---
+++ src/test/java/com/development/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

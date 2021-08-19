275e75d7c63d3f69261825308316a4b7078cb18b
Eric Balawejder <eric.balawejder@protonmail.com>
2021-08-19 14:28:22 -0400

    Add search for products by category feature. Define routes. Configure router. Define the router outlet. Create hard coded router links in menu. Enhance ProductListComponent to read category id param. Add RestRepository method in spring boot app. Update service to call new url on spring boot app.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ecommerce
 
 An ecommerce shopping application. Styles created with [bootstrap](https://getbootstrap.com/).
+
+[Lombok and JPA uses](https://dzone.com/articles/lombok-and-jpa-what-may-go-wrong)
--- src/main/resources/webapp/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ src/main/resources/webapp/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list-table.component.html',
-  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products!: Product[];
+  currentCategoryId!: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // Check if "id" param is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // Get the "id" param string. Convert string to a number using the + symbol
+      // @ts-ignore
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      // default when not available
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }

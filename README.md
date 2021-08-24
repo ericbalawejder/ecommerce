e5cb16076c72ab228a2d55b13cd02868515208ab
Eric Balawejder <eric.balawejder@protonmail.com>
2021-08-24 13:03:44 -0400

    Expose entity ids - modify spring boot app. Create ProductCategory class. Create new component for menu. Enhance menu component to read data from product service. Update product service to call URL on spring boot app. In HTML, replace hard coded links with menu component.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ecommerce
 
-An ecommerce shopping application. Styles created with [bootstrap](https://getbootstrap.com/).
+An ecommerce shopping application build with Spring Boot and [Angular](https://angular.io/guide/file-structure). 
+Styles created with [bootstrap](https://getbootstrap.com/).
 
 [Lombok and JPA uses](https://dzone.com/articles/lombok-and-jpa-what-may-go-wrong)
--- src/main/java/com/development/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/development/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.development.ecommerce.config;
 
 import com.development.ecommerce.entity.Product;
 import com.development.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,6 +42,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Call internal helper method to expose ids
+        exposeIds(config);
+    }
+
+    // Expose entity ids
+    private void exposeIds(RepositoryRestConfiguration repositoryRestConfiguration) {
+        // Get a list of all entity classes from entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // Create an array of the entities types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // Get the entity types for the entities
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+        // Expose the entities ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        repositoryRestConfiguration.exposeIdsFor(domainTypes);
     }
 
 }

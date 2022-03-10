db905ac32b2a52678dc6cd8f3dae6d0d0ecc86bd
Eric Balawejder <eric.balawejder@protonmail.com>
2022-03-09 21:36:48 -0500

    Create TypeScript classes for Country and State. Add methods to the form service for countries and states. Update checkout component to retrieve the countries from service. Update HTML template to populate drop-down lists for countries. Add event handler for checkout component. Read the selected country, retrieve list of states based on selected country. Update HTML template to populate drop-down lists for states.

--- src/main/resources/webapp/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ src/main/resources/webapp/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {ShopFormService} from "../../services/shop-form.service";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -10,17 +12,20 @@ import {ShopFormService} from "../../services/shop-form.service";
 export class CheckoutComponent implements OnInit {
 
   // @ts-ignore
-  checkoutFromGroup: FormGroup;
+  checkoutFormGroup: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder, private shopFormService: ShopFormService) {
   }
 
   ngOnInit(): void {
-    this.checkoutFromGroup = this.formBuilder.group({
+    this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
         lastName: [''],
@@ -65,26 +70,41 @@ export class CheckoutComponent implements OnInit {
         this.creditCardYears = data;
       }
     );
+
+    this.shopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    );
+
   }
 
   onSubmit() {
     console.log("Handling the submit button");
-    console.log(this.checkoutFromGroup.get('customer')?.value);
-    console.log("email address is: " + this.checkoutFromGroup.get('customer')?.value.email);
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log("email address is: " + this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("The shipping address country is " +
+      this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+
+    console.log("The shipping address state is " +
+      this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
     // @ts-ignore
     if (event.target.checked) {
-      this.checkoutFromGroup.controls.billingAddress
-        .setValue(this.checkoutFromGroup.controls.shippingAddress.value);
+      this.checkoutFormGroup.controls.billingAddress
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
-      this.checkoutFromGroup.controls.billingAddress.reset();
+      this.checkoutFormGroup.controls.billingAddress.reset();
+      this.billingAddressStates = [];
     }
   }
 
   handleMonthsAndYears() {
-    const creditCardFormGroup = this.checkoutFromGroup.get("creditCard");
+    const creditCardFormGroup = this.checkoutFormGroup.get("creditCard");
     const currentYear: number = new Date().getFullYear();
     const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
     let startMonth: number;
@@ -102,4 +122,21 @@ export class CheckoutComponent implements OnInit {
     );
   }
 
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+    console.log(`${formGroupName} country code : ${countryCode}`);
+    console.log(`${formGroupName} country name : ${countryName}`);
+    this.shopFormService.getStates(countryCode).subscribe(
+      data => {
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data
+        }
+        formGroup?.get('state')?.setValue(data[0]);
+      }
+    )
+  }
 }
--- src/main/resources/webapp/angular-ecommerce/src/app/services/shop-form.service.ts
+++ src/main/resources/webapp/angular-ecommerce/src/app/services/shop-form.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {State} from "../common/state";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
@@ -26,4 +48,16 @@ export class ShopFormService {
     return of(data);
   }
 
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
 }
